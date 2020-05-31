1cf499af4f592cb4ae1f435f1e55c1eae8170bd0
Benny Neugebauer <bn@bennyn.de>
2020-05-31 17:45:57 +0200

    feat: Add Moving Average Convergence Divergence (MACD) indicator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './DEMA/DEMA';
 export * from './DMA/DMA';
 export * from './EMA/EMA';
+export * from './MACD/MACD';
 export * from './ROC/ROC';
 export * from './RSI/RSI';
 export * from './SMA/SMA';
