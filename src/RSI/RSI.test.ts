123b24bd5b15df8db449141632eaf308559a9184
Benny Neugebauer <bn@bennyn.de>
2020-09-03 14:10:15 +0200

    Add RSI/EMA test case

--- src/RSI/RSI.test.ts
+++ src/RSI/RSI.test.ts
@@ -1,6 +1,8 @@
 import {Big as BigNumber} from 'big.js';
+import {EMA} from '..';
 import {RSI} from './RSI';
 
+import candles from '../test/fixtures/RSI/500-candles-WRX-BTC-1h.json';
 import prices from '../test/fixtures/prices.json';
 import results from '../test/fixtures/RSI/results.json';
 
@@ -35,6 +37,17 @@ describe('RSI', () => {
         const expected = new BigNumber(rsi26results[index]);
         expect(rsi.getResult().toPrecision(12)).toEqual(expected.toPrecision(12));
       });
+    });
+
+    fit('supports using EMAs for calculation', () => {
+      const rsi = new RSI(14, EMA);
+
+      for (const candle of Object.values(candles)) {
+        const closingPrice = candle[4];
+        rsi.update(closingPrice);
+      }
+
+      expect(rsi.getResult().valueOf()).toBe('70');
     });
 
     it('prevents division by zero errors when the average gain and average loss equal 0', () => {
