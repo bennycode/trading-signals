3828c197b093eb948cbcfadfdc8b5cd986428c75
Benny Neugebauer <bn@bennyn.de>
2021-02-16 00:24:15 +0100

    feat: Add Center of Gravity (CG) oscillator

--- src/CG/CG.ts
+++ src/CG/CG.ts
@@ -1,6 +1,7 @@
 import {SimpleIndicator} from '../Indicator';
 import Big, {BigSource} from 'big.js';
-import {NotEnoughDataError} from '../../dist';
+import {SMA} from '../SMA/SMA';
+import {NotEnoughDataError} from '../error';
 
 /**
  * Implementation of the Center of Gravity (CG) oscillator by John Ehlers.
@@ -8,29 +9,29 @@ import {NotEnoughDataError} from '../../dist';
  * @note According to the specification, the price inputs shall be calculated the following way:
  * ((High Price + Low Price) / 2)
  * @note The selected interval should be half the dominant cycle length
- * @note If the interval gets too short, the CG oscillator loses its smoothing and gets a little too nervous for profitable trading
+ * @note If the interval gets too short, the CG oscillator loses its smoothing and gets a little too nervous for
+ *   profitable trading
  * @see http://www.mesasoftware.com/papers/TheCGOscillator.pdf
  */
 export class CG implements SimpleIndicator {
+  public signal: SMA;
+
   private readonly prices: Big[] = [];
+  private result: Big | undefined;
 
   get isStable(): boolean {
-    return this.prices.length >= this.interval;
+    return this.prices.length >= this.interval && this.signal.isStable;
   }
 
-  constructor(public readonly interval: number) {}
+  constructor(public readonly interval: number, public readonly signalInterval: number) {
+    this.signal = new SMA(signalInterval);
+  }
 
   update(price: BigSource): void {
     this.prices.push(new Big(price));
 
     if (this.prices.length > this.interval) {
       this.prices.shift();
-    }
-  }
-
-  getResult(): Big {
-    if (!this.isStable) {
-      throw new NotEnoughDataError();
     }
 
     let nominator = new Big(0);
@@ -42,9 +43,18 @@ export class CG implements SimpleIndicator {
       denominator = denominator.plus(price);
     }
 
-    if (denominator.gt(0)) {
-      return nominator.mul(-1).div(denominator);
+    const cg = denominator.gt(0) ? nominator.div(denominator) : new Big(0);
+
+    this.signal.update(cg);
+
+    this.result = cg;
+  }
+
+  getResult(): Big {
+    if (!this.result) {
+      throw new NotEnoughDataError();
     }
-    return new Big(0);
+
+    return this.result;
   }
 }
