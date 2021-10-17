bba3b267faad88220a1d265e65ea7a00528d6626
Benny Neugebauer <mail@bennycode.com>
2021-10-17 18:08:15 +0200

    test: Verify test data with other libraries (#334)

--- src/BBANDS/BollingerBands.test.ts
+++ src/BBANDS/BollingerBands.test.ts
@@ -61,5 +61,83 @@ describe('BollingerBands', () => {
         expect(error).toBeInstanceOf(NotEnoughDataError);
       }
     });
+
+    it('is compatible with results from Tulip Indicators (TI)', () => {
+      // Test data taken from:
+      // https://tulipindicators.org/bbands
+      const inputs = [
+        81.59, 81.06, 82.87, 83.0, 83.61, 83.15, 82.84, 83.99, 84.55, 84.36, 85.53, 86.54, 86.89, 87.77, 87.29,
+      ];
+
+      const expectedLows = [
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        '80.53',
+        '80.99',
+        '82.53',
+        '82.47',
+        '82.42',
+        '82.44',
+        '82.51',
+        '83.14',
+        '83.54',
+        '83.87',
+        '85.29',
+      ];
+
+      const expectedMids = [
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        '82.43',
+        '82.74',
+        '83.09',
+        '83.32',
+        '83.63',
+        '83.78',
+        '84.25',
+        '84.99',
+        '85.57',
+        '86.22',
+        '86.80',
+      ];
+
+      const expectedUps = [
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        '84.32',
+        '84.49',
+        '83.65',
+        '84.16',
+        '84.84',
+        '85.12',
+        '86.00',
+        '86.85',
+        '87.61',
+        '88.57',
+        '88.32',
+      ];
+
+      const bb = new BollingerBands(5, 2);
+
+      for (let i = 0; i < inputs.length; i++) {
+        const price = inputs[i];
+        bb.update(price);
+        if (bb.isStable) {
+          const {lower, middle, upper} = bb.getResult();
+          const expectedLow = expectedLows[i];
+          const expectedMid = expectedMids[i];
+          const expectedUp = expectedUps[i];
+          expect(lower.toFixed(2)).toBe(`${expectedLow}`);
+          expect(middle.toFixed(2)).toBe(`${expectedMid}`);
+          expect(upper.toFixed(2)).toBe(`${expectedUp}`);
+        }
+      }
+    });
   });
 });
--- src/STOCH/StochasticOscillator.test.ts
+++ src/STOCH/StochasticOscillator.test.ts
@@ -4,7 +4,8 @@ import {NotEnoughDataError} from '../error';
 describe('StochasticOscillator', () => {
   describe('update', () => {
     it('is stable when the amount of inputs is bigger than the required interval', () => {
-      // Test vectors taken from: https://runkit.com/anandaravindan/stochastic
+      // Test data taken from:
+      // https://runkit.com/anandaravindan/stochastic
       const highs = [
         127.009, 127.616, 126.591, 127.347, 128.173, 128.432, 127.367, 126.422, 126.9, 126.85, 125.646, 125.716,
         127.158, 127.715, 127.686, 128.223, 128.273, 128.093, 128.273, 127.735, 128.77, 129.287, 130.063, 129.118,
