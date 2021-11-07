32aecd830e9666c4be204a4402860207d6ccfbd0
Benny Neugebauer <bn@bennyn.de>
2021-11-07 18:41:53 +0100

    test(MA): Add tests for moving average base class

---
+++ src/MA/MovingAverage.test.ts
@@ -0,0 +1,27 @@
+import {FasterMovingAverage} from './MovingAverage';
+
+class MyAverage extends FasterMovingAverage {
+  iterations = 0;
+  total = 0;
+
+  update(price: number): number | void {
+    if (!this.result) {
+      this.result = 0;
+    }
+    this.iterations += 1;
+    this.total += price;
+    return (this.result = this.total / this.iterations);
+  }
+}
+
+describe('FasterMovingAverage', () => {
+  it('can be used to implement custom average calculations based on primitive numbers', () => {
+    const average = new MyAverage(Infinity);
+    expect(average.isStable).toBeFalse();
+    expect(() => average.getResult()).toThrowError();
+    average.update(50);
+    average.update(100);
+    const result = average.getResult();
+    expect(result).toBe(75);
+  });
+});
