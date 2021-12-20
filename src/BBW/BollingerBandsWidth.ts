284bb90ec7d1b7fb28d0e63ebecfc18006d9bccc
Benny Neugebauer <bn@bennyn.de>
2021-12-21 00:59:15 +0100

    feat(BBANDS,BBW): Add Bollinger Bands Width (BBW)

--- src/BBW/BollingerBandsWidth.ts
+++ src/BBW/BollingerBandsWidth.ts
@@ -4,8 +4,8 @@ import {BollingerBands, FasterBollingerBands} from '../BBANDS/BollingerBands';
 
 /**
  * The Bollinger Bands Width (BBW) indicator, developed by John A. Bollinger, merges the information of Bollinger Bands
- * into one definite number. It represents the difference between the Upper Band and the Lower Band to define the
- * narrowness of the underlying Bollinger Bands.
+ * into one definite number. It defines the narrowness of the underlying Bollinger Bands by representing the difference
+ * between the Upper Band and the Lower Band.
  *
  * @see https://www.tradingview.com/support/solutions/43000501972-bollinger-bands-width-bbw/
  */
