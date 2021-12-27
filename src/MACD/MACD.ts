9d330d5dd211b375f2dd8cb1b476297025912b30
Benny Neugebauer <mail@bennycode.com>
2021-12-27 18:01:00 +0100

    feat(MACD): Add faster implementation (#384)

--- src/MACD/MACD.ts
+++ src/MACD/MACD.ts
@@ -1,6 +1,6 @@
-import {EMA} from '../EMA/EMA';
+import {EMA, FasterEMA} from '../EMA/EMA';
 import Big, {BigSource} from 'big.js';
-import {DEMA, NotEnoughDataError} from '..';
+import {DEMA, FasterDEMA, NotEnoughDataError} from '..';
 import {Indicator} from '../Indicator';
 
 export type MACDConfig = {
@@ -16,59 +16,72 @@ export type MACDResult = {
   signal: Big;
 };
 
+export type FasterMACDResult = {
+  histogram: number;
+  macd: number;
+  signal: number;
+};
+
 /**
  * Moving Average Convergence Divergence (MACD)
  * Type: Momentum
  *
- * The MACD triggers trading signals when it crosses above (bullish buying opportunity) or below (bearish selling opportunity) its signal line. MACD can be used together with the RSI to provide a more accurate trading signal.
+ * The MACD triggers trading signals when it crosses above (bullish buying opportunity) or below (bearish selling
+ * opportunity) its signal line. MACD can be used together with the RSI to provide a more accurate trading signal.
  *
  * @see https://www.investopedia.com/terms/m/macd.asp
  */
 export class MACD implements Indicator<MACDResult> {
+  public readonly prices: BigSource[] = [];
   public readonly long: EMA | DEMA;
   public readonly short: EMA | DEMA;
 
   private readonly signal: EMA | DEMA;
-  private age: number = 0;
   private result: MACDResult | undefined;
 
-  constructor(private readonly config: MACDConfig) {
+  constructor(config: MACDConfig) {
     this.long = new config.indicator(config.longInterval);
     this.short = new config.indicator(config.shortInterval);
     this.signal = new config.indicator(config.signalInterval);
   }
 
   get isStable(): boolean {
-    return this.age >= this.config.longInterval;
+    return this.result !== undefined;
   }
 
-  update(_price: BigSource): void {
+  update(_price: BigSource): void | MACDResult {
     const price = new Big(_price);
+    this.prices.push(price);
 
-    const shortEMA = this.short.update(price);
-    const longEMA = this.long.update(price);
-    this.age++;
+    const short = this.short.update(price);
+    const long = this.long.update(price);
 
-    /**
-     * Standard MACD is the short (usually 12 periods) EMA less the long (usually 26 periods) EMA. Closing prices are
-     * used to form the moving averages.
-     */
-    const macd = shortEMA.sub(longEMA);
+    if (this.prices.length > this.long.interval) {
+      this.prices.shift();
+    }
 
-    /**
-     * A short (usually 9 periods) EMA of MACD is plotted along side to act as a signal line to identify turns in the
-     * indicator. It gets updated once the long EMA has enough input data.
-     */
-    const signalLine = this.isStable ? this.signal.update(macd) : new Big(0);
+    if (this.prices.length === this.long.interval) {
+      /**
+       * Standard MACD is the short (usually 12 periods) EMA less the long (usually 26 periods) EMA. Closing prices are
+       * used to form the moving averages.
+       */
+      const macd = short.sub(long);
 
-    /**
-     * The MACD histogram is calculated as the MACD indicator minus the signal line (usually 9 periods) EMA.
-     */
-    this.result = {
-      histogram: macd.sub(signalLine),
-      macd: macd,
-      signal: signalLine,
-    };
+      /**
+       * A short (usually 9 periods) EMA of MACD is plotted along side to act as a signal line to identify turns in the
+       * indicator. It gets updated once the long EMA has enough input data.
+       */
+      const signal = this.signal.update(macd);
+
+      /**
+       * The MACD histogram is calculated as the MACD indicator minus the signal line (usually 9 periods) EMA.
+       */
+      return (this.result = {
+        histogram: macd.sub(signal),
+        macd: macd,
+        signal,
+      });
+    }
   }
 
   getResult(): MACDResult {
@@ -77,5 +90,50 @@ export class MACD implements Indicator<MACDResult> {
     }
 
     return this.result;
+  }
+}
+
+export class FasterMACD implements Indicator<FasterMACDResult> {
+  public readonly prices: number[] = [];
+  private result: FasterMACDResult | undefined;
+
+  constructor(
+    public readonly short: FasterEMA | FasterDEMA,
+    public readonly long: FasterEMA | FasterDEMA,
+    public readonly signal: FasterEMA | FasterDEMA
+  ) {}
+
+  getResult(): FasterMACDResult {
+    if (this.result === undefined) {
+      throw new NotEnoughDataError();
+    }
+
+    return this.result;
+  }
+
+  get isStable(): boolean {
+    return this.result !== undefined;
+  }
+
+  update(price: number): void | FasterMACDResult {
+    this.prices.push(price);
+
+    const short = this.short.update(price);
+    const long = this.long.update(price);
+
+    if (this.prices.length > this.long.interval) {
+      this.prices.shift();
+    }
+
+    if (this.prices.length === this.long.interval) {
+      const macd = short - long;
+      const signal = this.signal.update(macd);
+
+      return (this.result = {
+        histogram: macd - signal,
+        macd,
+        signal,
+      });
+    }
   }
 }
