5ef9ed36e47e382cd685a82d07bba7cb68c01525
Benny Neugebauer <mail@bennycode.com>
2023-10-11 23:08:41 +0200

    docs: Describe ATR values

--- src/ATR/ATR.ts
+++ src/ATR/ATR.ts
@@ -14,6 +14,18 @@ import {FasterWSMA, WSMA} from '../WSMA/WSMA';
  * enthusiasm of traders. Large or increasing ranges suggest traders prepared to continue to bid up or sell down a
  * stock through the course of the day. Decreasing range indicates declining interest.
  *
+ * A stock with a higher ATR is indicative of increased volatility, while a lower ATR suggests decreased volatility
+ * during the assessed time frame.
+ *
+ * - Low ATR (e.g., 0.5 to 1): Typically associated with low-volatility stocks or markets. Prices tend to move in a
+ * relatively calm and steady manner.
+ *
+ * - Moderate ATR (e.g., 1 to 2): Indicates moderate volatility. Prices may experience periodic fluctuations, but they
+ * are not extreme. Many traders find stocks with ATR around 2 to be suitable for trading with manageable risk.
+ *
+ * - High ATR (e.g., 2 or higher): Suggests higher volatility. Stocks with ATR values greater than 2 are prone to more
+ * significant price swings, and they may exhibit larger price movements.
+ *
  * @see https://www.investopedia.com/terms/a/atr.asp
  */
 export class ATR extends BigIndicatorSeries<HighLowClose> {
