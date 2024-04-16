041cedade55d454920a289b3626beb12223f4472
Özüm Eldoğan <ozum@ozum.net>
2024-04-16 11:42:43 +0300

    fix(TR): Correct replace feature (#666)

--- src/TR/TR.ts
+++ src/TR/TR.ts
@@ -15,33 +15,50 @@ import {getMaximum, HighLowClose, HighLowCloseNumber} from '../util/index.js';
  */
 export class TR extends BigIndicatorSeries<HighLowClose> {
   private previousCandle?: HighLowClose;
+  private twoPreviousCandle?: HighLowClose;
 
   update(candle: HighLowClose, replace: boolean = false): Big {
     const high = new Big(candle.high);
     const highLow = high.minus(candle.low);
+
+    if (this.previousCandle && replace) {
+      this.previousCandle = this.twoPreviousCandle;
+    }
+
     if (this.previousCandle) {
       const highClose = high.minus(this.previousCandle.close).abs();
       const lowClose = new Big(candle.low).minus(this.previousCandle.close).abs();
+      this.twoPreviousCandle = this.previousCandle;
       this.previousCandle = candle;
       return this.setResult(getMaximum([highLow, highClose, lowClose]), replace);
     }
+    this.twoPreviousCandle = this.previousCandle;
     this.previousCandle = candle;
+
     return this.setResult(highLow, replace);
   }
 }
 
 export class FasterTR extends NumberIndicatorSeries<HighLowCloseNumber> {
   private previousCandle?: HighLowCloseNumber;
+  private twoPreviousCandle?: HighLowCloseNumber;
 
   update(candle: HighLowCloseNumber, replace: boolean = false): number {
     const {high, low} = candle;
     const highLow = high - low;
+
+    if (this.previousCandle && replace) {
+      this.previousCandle = this.twoPreviousCandle;
+    }
+
     if (this.previousCandle) {
       const highClose = Math.abs(high - this.previousCandle.close);
       const lowClose = Math.abs(low - this.previousCandle.close);
+      this.twoPreviousCandle = this.previousCandle;
       this.previousCandle = candle;
       return this.setResult(Math.max(highLow, highClose, lowClose), replace);
     }
+    this.twoPreviousCandle = this.previousCandle;
     this.previousCandle = candle;
     return this.setResult(highLow, replace);
   }
