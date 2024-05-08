a0bc5705e9aa8a2b9e0fed9414ad9deb9694c287
Benny Neugebauer <mail@bennycode.com>
2024-05-08 16:40:17 +0200

    feat(WMA): Add WMA to moving average types (#685)

--- src/MA/MovingAverageTypes.ts
+++ src/MA/MovingAverageTypes.ts
@@ -1,6 +1,7 @@
 import type {EMA, FasterEMA} from '../EMA/EMA.js';
 import type {FasterWSMA, WSMA} from '../WSMA/WSMA.js';
 import type {FasterSMA, SMA} from '../SMA/SMA.js';
+import type {FasterWMA, WMA} from '../WMA/WMA.js';
 
-export type MovingAverageTypes = typeof EMA | typeof SMA | typeof WSMA;
-export type FasterMovingAverageTypes = typeof FasterEMA | typeof FasterSMA | typeof FasterWSMA;
+export type MovingAverageTypes = typeof EMA | typeof SMA | typeof WMA | typeof WSMA;
+export type FasterMovingAverageTypes = typeof FasterEMA | typeof FasterSMA | typeof FasterWMA | typeof FasterWSMA;
