6c2b9553329df891f785dd24fcde19e4fe10b853
Benny Neugebauer <mail@bennycode.com>
2024-11-25 15:30:05 +0100

    add percentage

--- src/util/getStreaks.test.ts
+++ src/util/getStreaks.test.ts
@@ -3,13 +3,23 @@ import {getStreaks} from './getStreaks.js';
 describe('getStreaks', () => {
   const input = [10, 20, 30, 40, 32, 42, 50, 45, 44, 41, 59, 90, 100];
 
-  it('keeps an array of upward combinations', () => {
+  it('keeps track of upward streak lengths', () => {
     const actual = getStreaks(input, 'up');
-    expect(actual).toStrictEqual([3, 2, 3]);
+    expect(actual.map(s => s.length)).toStrictEqual([3, 2, 3]);
   });
 
-  it('keeps an array of upward combinations', () => {
+  it('keeps track of price increases during an upward streak', () => {
+    const actual = getStreaks(input, 'up');
+    expect(actual.map(s => s.percentage)).toStrictEqual([300, 56.25, 143.90243902439025]);
+  });
+
+  it('keeps track of downward streak lengths', () => {
     const actual = getStreaks(input, 'down');
-    expect(actual).toStrictEqual([1, 3]);
+    expect(actual.map(s => s.length)).toStrictEqual([1, 3]);
+  });
+
+  it('keeps track of price decreases during a downward streak', () => {
+    const actual = getStreaks(input, 'down');
+    expect(actual.map(s => s.percentage)).toStrictEqual([-20, -18]);
   });
 });
--- src/util/getStreaks.ts
+++ src/util/getStreaks.ts
@@ -1,6 +1,19 @@
-export function getStreaks(prices: number[], side: 'up' | 'down') {
-  const streaks: number[] = [];
+export type Streak = {
+  length: number;
+  /** Price change percentage during the streak */
+  percentage: number;
+};
+
+export function getStreaks(prices: number[], side: 'up' | 'down'): Streak[] {
+  const streaks: Streak[] = [];
   let currentStreak = 0;
+
+  function saveStreak(i: number) {
+    const endPrice = prices[i - 1];
+    const startPrice = prices[i - currentStreak - 1];
+    const percentage = ((endPrice - startPrice) / startPrice) * 100;
+    streaks.push({length: currentStreak, percentage});
+  }
 
   for (let i = 1; i < prices.length; i++) {
     const isUpward = side === 'up' && prices[i] > prices[i - 1];
@@ -10,7 +23,7 @@ export function getStreaks(prices: number[], side: 'up' | 'down') {
     } else {
       // Save the streak if it ends
       if (currentStreak > 0) {
-        streaks.push(currentStreak);
+        saveStreak(i);
       }
       // Reset the streak
       currentStreak = 0;
@@ -19,7 +32,7 @@ export function getStreaks(prices: number[], side: 'up' | 'down') {
 
   // Append the final streak if it exists
   if (currentStreak > 0) {
-    streaks.push(currentStreak);
+    saveStreak(prices.length);
   }
 
   return streaks;
