c60011c3e087bc8b0438636dde438e02eb0ca66d
Benny Neugebauer <mail@bennycode.com>
2024-12-06 12:51:50 +0100

    fix thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,15 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      branches: 100,
-      functions: 100,
       include: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/cli.ts', '!**/index.ts', '!**/start*.ts'],
-      lines: 100,
       provider: 'v8',
       reporter: ['html', 'lcov', 'text'],
-      statements: 100,
+      thresholds: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
     },
     environment: 'node',
     globals: true,
