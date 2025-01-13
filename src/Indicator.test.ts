34f4703a3cb76d2b2671da2610bb00a71d4b6132
Benny Neugebauer <mail@bennycode.com>
2025-01-13 23:36:52 +0100

    typo

--- src/Indicator.test.ts
+++ src/Indicator.test.ts
@@ -109,7 +109,7 @@ describe('Indicator', () => {
   });
 
   describe('updates', () => {
-    it('returns all values from multiple updates', () => {
+    it('returns all results from multiple updates', () => {
       const itc = new IndicatorTestClass();
       const results = itc.updates([100, 1_000, 10_000]);
       expect(results.map(big => big?.toString())).toEqual(['100', '550', '3700']);
