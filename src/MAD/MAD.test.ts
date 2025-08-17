965b5010318255cdb31b3673a02fbf5b484065ae
Benny Neugebauer <mail@bennycode.com>
2025-08-17 19:55:37 +0200

    fix MAD

--- src/MAD/MAD.test.ts
+++ src/MAD/MAD.test.ts
@@ -6,25 +6,37 @@ describe('MAD', () => {
   // https://tulipindicators.org/md
   const prices = [
     81.59, 81.06, 82.87, 83.0, 83.61, 83.15, 82.84, 83.99, 84.55, 84.36, 85.53, 86.54, 86.89, 87.77, 87.29,
-  ];
-  const expectations = ['0.88', '0.67', '0.23', '0.39', '0.51', '0.63', '0.67', '0.83', '0.91', '1.02', '0.62'];
+  ] as const;
+  const expectations = [
+    '0.88',
+    '0.67',
+    '0.23',
+    '0.39',
+    '0.51',
+    '0.63',
+    '0.67',
+    '0.83',
+    '0.91',
+    '1.02',
+    '0.62',
+  ] as const;
 
   describe('update', () => {
     it('can replace recently added values', () => {
-      const fasterMAD = new MAD(5);
+      const mad = new MAD(5);
 
-      fasterMAD.add(81.59);
-      fasterMAD.add(81.06);
-      fasterMAD.add(82.87);
-      fasterMAD.add(83.0);
-      fasterMAD.add(83.61);
-      fasterMAD.add(83.15);
-      fasterMAD.add(82.84);
-      fasterMAD.add(90);
-      fasterMAD.update(83.99, true);
+      mad.add(81.59);
+      mad.add(81.06);
+      mad.add(82.87);
+      mad.add(83.0);
+      mad.add(83.61);
+      mad.add(83.15);
+      mad.add(82.84);
+      mad.add(90);
+      mad.update(83.99, true);
 
-      expect(fasterMAD.isStable).toBe(true);
-      expect(fasterMAD.getResultOrThrow().toFixed(2)).toBe('0.39');
+      expect(mad.isStable).toBe(true);
+      expect(mad.getResultOrThrow().toFixed(2)).toBe('0.39');
     });
   });
 
@@ -33,46 +45,43 @@ describe('MAD', () => {
       // Test data verified with:
       // https://en.wikipedia.org/wiki/Average_absolute_deviation#Mean_absolute_deviation_around_a_central_point
       const prices = [2, 2, 3, 4, 14];
-      const fasterMAD = new MAD(5);
+      const mad = new MAD(5);
+
       for (const price of prices) {
-        fasterMAD.add(price);
+        mad.add(price);
       }
-      expect(fasterMAD.getResultOrThrow().valueOf()).toBe(3.6);
+
+      expect(mad.getResultOrThrow().valueOf()).toBe(3.6);
     });
 
     it('is compatible with results from Tulip Indicators (TI)', () => {
-      const fasterMAD = new MAD(5);
-      for (const price of prices) {
-        fasterMAD.add(price);
-        if (fasterMAD.isStable) {
-          const expected = expectations.shift()!;
-          expect(fasterMAD.getResultOrThrow().toFixed(2)).toBe(expected);
+      const mad = new MAD(5);
+      const offset = mad.getRequiredInputs() - 1;
+
+      prices.forEach((price, i) => {
+        mad.add(price);
+        if (mad.isStable) {
+          const expected = expectations[i - offset];
+          expect(mad.getResultOrThrow().toFixed(2)).toBe(expected);
         }
-      }
-      expect(fasterMAD.getResultOrThrow().toFixed(2)).toBe('0.62');
+      });
+
+      expect(mad.getResultOrThrow().toFixed(2)).toBe('0.62');
     });
 
     it("stores the highest and lowest result throughout the indicator's lifetime", () => {
-      const fasterMAD = new MAD(5);
+      const mad = new MAD(5);
       for (const price of prices) {
-        fasterMAD.add(price);
+        mad.add(price);
       }
-      expect(fasterMAD.highest?.toFixed(4)).toBe('1.0184');
-      expect(fasterMAD.lowest?.toFixed(4)).toBe('0.2288');
+      expect(mad.highest?.toFixed(4)).toBe('1.0184');
+      expect(mad.lowest?.toFixed(4)).toBe('0.2288');
     });
 
     it('throws an error when there is not enough input data', () => {
       const mad = new MAD(5);
       try {
         mad.getResultOrThrow();
-        throw new Error('Expected error');
-      } catch (error) {
-        expect(error).toBeInstanceOf(NotEnoughDataError);
-      }
-
-      const fasterMAD = new MAD(5);
-      try {
-        fasterMAD.getResultOrThrow();
         throw new Error('Expected error');
       } catch (error) {
         expect(error).toBeInstanceOf(NotEnoughDataError);
