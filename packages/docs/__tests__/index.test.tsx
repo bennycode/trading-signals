3b62f5fe8964e4a581fb73b2fcaff3f887130378
Benny Neugebauer <mail@bennycode.com>
2025-12-02 00:51:57 +0100

    fix(docs): found page without a React Component as default export

--- packages/docs/__tests__/index.test.tsx
+++ packages/docs/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import {describe, expect, it} from 'vitest';
-import Home from './index';
+import Home from '../pages/index';
 
 describe('Home Page', () => {
   it('renders all category cards', () => {
