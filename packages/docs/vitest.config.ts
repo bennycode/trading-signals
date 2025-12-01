d83567c72d289ace2f8f4d8bae509c57fdd26714
Benny Neugebauer <mail@bennycode.com>
2025-12-02 00:41:09 +0100

    Use jsdom

--- packages/docs/vitest.config.ts
+++ packages/docs/vitest.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'happy-dom',
+    environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
   },
