875c4510d3c14a3975ac50fffafb630ad7193040
Benny Neugebauer <mail@bennycode.com>
2025-12-31 10:58:40 +0100

    add startServer

--- packages/messaging/src/start.ts
+++ packages/messaging/src/start.ts
@@ -1,33 +1,4 @@
 import 'dotenv-defaults/config.js';
+import {startServer} from './startServer.js';
 
-import {Agent} from '@xmtp/agent-sdk';
-import {getTestUrl} from '@xmtp/agent-sdk/debug';
-import {CommandRouter} from '@xmtp/agent-sdk/middleware';
-import time from './command/time.js';
-import candle from './command/candle.js';
-import {isFromOwner} from './middleware/isFromOwner.js';
-
-const router = new CommandRouter();
-
-router.command('/candle', async ctx => {
-  await ctx.conversation.send(await candle(ctx.message.content));
-});
-
-router.command('/time', async ctx => {
-  await ctx.conversation.send(await time());
-});
-
-const agent = await Agent.createFromEnv({
-  appVersion: '@typedtrader/messaging',
-});
-
-agent.on('start', ctx => {
-  console.log(`Message me: ${getTestUrl(ctx.client)}`);
-});
-
-if (process.env.XMTP_OWNER_ADDRESS) {
-  agent.use(isFromOwner);
-}
-agent.use(router.middleware());
-await agent.client.revokeAllOtherInstallations();
-await agent.start();
+await startServer();
---
+++ packages/messaging/src/startServer.ts
@@ -0,0 +1,33 @@
+import {Agent} from '@xmtp/agent-sdk';
+import {getTestUrl} from '@xmtp/agent-sdk/debug';
+import {CommandRouter} from '@xmtp/agent-sdk/middleware';
+import time from './command/time.js';
+import candle from './command/candle.js';
+import {isFromOwner} from './middleware/isFromOwner.js';
+
+export async function startServer() {
+  const router = new CommandRouter();
+
+  router.command('/candle', async ctx => {
+    await ctx.conversation.send(await candle(ctx.message.content));
+  });
+
+  router.command('/time', async ctx => {
+    await ctx.conversation.send(await time());
+  });
+
+  const agent = await Agent.createFromEnv({
+    appVersion: '@typedtrader/messaging',
+  });
+
+  agent.on('start', ctx => {
+    console.log(`Message me: ${getTestUrl(ctx.client)}`);
+  });
+
+  if (process.env.XMTP_OWNER_ADDRESS) {
+    agent.use(isFromOwner);
+  }
+  agent.use(router.middleware());
+  await agent.client.revokeAllOtherInstallations();
+  await agent.start();
+}
