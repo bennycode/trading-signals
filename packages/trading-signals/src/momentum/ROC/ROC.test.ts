1ee7b7cd8f830da20bc5b64bad48601ff72d0345
JAKE <96793691+MottledTool@users.noreply.github.com>
2026-01-14 19:02:21 -0400

    fix(trading-signals): Repair replace functionality for ROC (#925)

--- packages/trading-signals/src/momentum/ROC/ROC.test.ts
+++ packages/trading-signals/src/momentum/ROC/ROC.test.ts
@@ -125,4 +125,28 @@ describe('ROC', () => {
       expect(signal.state).toBe(TradingSignal.BULLISH);
     });
   });
+
+  describe('update', () => {
+    it('returns a valid result when update with replace==true', () => {
+      // Test data verified with:
+      // https://tulipindicators.org/roc
+      const prices = [81.59, 81.06, 82.87, 83.0, 83.61] as const;
+      const updatePrice = 82.84;
+      const expectation = 0.01911999019;
+
+      const interval = 5;
+      const roc = new ROC(interval);
+
+      // add values until stable:
+      prices.forEach(price => {
+        roc.add(price);
+      });
+
+      // update the last value:
+      roc.update(updatePrice, true);
+
+      // validate the result:
+      expect(roc.getResultOrThrow().toFixed(2)).toEqual(expectation.toFixed(2));
+    });
+  });
 });
