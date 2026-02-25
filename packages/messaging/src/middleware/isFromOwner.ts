241d74d65453724a9ed2a8a88701df33d67a177a
Benny Neugebauer <mail@bennycode.com>
2026-02-25 01:08:03 +0100

    feat(@typedtrader/messaging): Allow messages only from owners (#980)

--- packages/messaging/src/middleware/isFromOwner.ts
+++ packages/messaging/src/middleware/isFromOwner.ts
@@ -1,22 +1,19 @@
 import {AgentMiddleware} from '@xmtp/agent-sdk';
 
-export const isFromOwner: AgentMiddleware = async (ctx, next) => {
-  const ownerAddress = process.env.XMTP_OWNER_ADDRESS;
+export const isFromOwner: (ownerAddresses: string) => AgentMiddleware = ownerAddresses => {
+  const addresses = ownerAddresses.split(',').map(a => a.trim());
+  return async (ctx, next) => {
+    const senderAddress = await ctx.getSenderAddress();
 
-  if (!ownerAddress) {
-    throw new Error(`Owner middleware requires XMTP_OWNER_ADDRESS environment variable to be set.`);
-  }
-
-  const senderAddress = await ctx.getSenderAddress();
-
-  if (senderAddress === ownerAddress) {
-    // Continue message processing
-    await next();
-  } else {
-    // Block message processing
-    console.warn(
-      `Ignoring message from "${senderAddress}" - only messages from owner "${ownerAddress}" are processed.`
-    );
-    return;
-  }
+    if (senderAddress && addresses.includes(senderAddress)) {
+      // Continue message processing
+      await next();
+    } else {
+      // Block message processing
+      console.warn(
+        `Ignoring message from "${senderAddress}" - only messages from owners [${addresses.join(', ')}] are processed.`
+      );
+      return;
+    }
+  };
 };
