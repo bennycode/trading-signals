5ca47c6e6c9a2f5991276373f15496d2f6a116ae
Benny Neugebauer <mail@bennycode.com>
2026-02-25 20:02:00 +0100

    fix(@typedtrader/messaging): Fix timestamp comparison in candle watcher

---
+++ packages/messaging/migrations/0002_moaning_kingpin.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_watches` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`accountId` integer NOT NULL,
+	`pair` text NOT NULL,
+	`intervalMs` integer NOT NULL,
+	`thresholdType` text NOT NULL,
+	`thresholdDirection` text NOT NULL,
+	`thresholdValue` text NOT NULL,
+	`baselinePrice` text NOT NULL,
+	`alertPrice` text NOT NULL,
+	`createdAt` text DEFAULT (CURRENT_TIMESTAMP),
+	FOREIGN KEY (`accountId`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+INSERT INTO `__new_watches`("id", "accountId", "pair", "intervalMs", "thresholdType", "thresholdDirection", "thresholdValue", "baselinePrice", "alertPrice", "createdAt") SELECT "id", "accountId", "pair", "intervalMs", "thresholdType", "thresholdDirection", "thresholdValue", "baselinePrice", "alertPrice", "createdAt" FROM `watches`;--> statement-breakpoint
+DROP TABLE `watches`;--> statement-breakpoint
+ALTER TABLE `__new_watches` RENAME TO `watches`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
