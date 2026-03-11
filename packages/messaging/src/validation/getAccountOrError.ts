4b4a433b74ba8fc044cc7d4ea5cf2a8351760244
Benny Neugebauer <mail@bennycode.com>
2026-03-11 15:31:32 +0100

    refactor(messaging): Rename ownerAddress to userId in getAccountOrError

--- packages/messaging/src/validation/getAccountOrError.ts
+++ packages/messaging/src/validation/getAccountOrError.ts
@@ -1,7 +1,7 @@
 import {Account} from '../database/models/Account.js';
 
-export const getAccountOrError = (ownerAddress: string, accountId: number) => {
-  const account = Account.findByOwnerAddressAndId(ownerAddress, accountId);
+export const getAccountOrError = (userId: string, accountId: number) => {
+  const account = Account.findByUserIdAndId(userId, accountId);
 
   if (!account) {
     throw new Error(`Account with ID "${accountId}" not found or does not belong to you`);
