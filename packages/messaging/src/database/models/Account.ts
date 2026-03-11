1d628629051b0bdde7229fe7a98f49e750239c1d
Benny Neugebauer <mail@bennycode.com>
2026-03-11 15:31:25 +0100

    refactor(messaging): Rename ownerAddress to userId in Account model

--- packages/messaging/src/database/models/Account.ts
+++ packages/messaging/src/database/models/Account.ts
@@ -14,20 +14,20 @@ export class Account {
     return db.select().from(accounts).where(eq(accounts.id, id)).get();
   }
 
-  static findByOwnerAddress(ownerAddress: string): AccountType[] {
+  static findByUserId(userId: string): AccountType[] {
     return db
       .select()
       .from(accounts)
-      .where(eq(accounts.ownerAddress, ownerAddress))
+      .where(eq(accounts.userId, userId))
       .orderBy(asc(accounts.id))
       .all();
   }
 
-  static findByOwnerAddressAndId(ownerAddress: string, id: number): AccountType | undefined {
+  static findByUserIdAndId(userId: string, id: number): AccountType | undefined {
     return db
       .select()
       .from(accounts)
-      .where(and(eq(accounts.ownerAddress, ownerAddress), eq(accounts.id, id)))
+      .where(and(eq(accounts.userId, userId), eq(accounts.id, id)))
       .get();
   }
 
