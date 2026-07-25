0dbef2b2502a5e5091e5019113158cf692636ea8
Benny Neugebauer <mail@bennycode.com>
2026-07-26 00:18:58 +0200

    chore: migrate from ESLint and Prettier to Oxlint and oxfmt (#1233)

---
+++ oxfmt.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'oxfmt';
+
+export default defineConfig({
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  printWidth: 120,
+  proseWrap: 'never',
+  semi: true,
+  singleQuote: true,
+  sortPackageJson: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+});
--- packages/trading-strategies/eslint.config.ts
+++
@@ -1,3 +0,0 @@
-import {createConfig} from '../../eslint.config.base.ts';
-
-export default createConfig();
