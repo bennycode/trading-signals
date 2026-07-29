7420988e9753ea6d7a7b3a3db026d687c7ead26a
Benny Neugebauer <mail@bennycode.com>
2026-07-29 13:35:53 +0200

    fix(exchange): remove console noise and dotenv-defaults from consumer install (#1240)

--- packages/exchange/src/broker/alpaca/getAlpacaMarketData.ts
+++ packages/exchange/src/broker/alpaca/getAlpacaMarketData.ts
@@ -1,8 +1,6 @@
 import {AlpacaMarketData, type AlpacaMarketDataOptions} from './AlpacaMarketData.js';
 
 export function getAlpacaMarketData(options: AlpacaMarketDataOptions) {
-  console.log('Initializing Alpaca market data');
-
   const marketData = new AlpacaMarketData(options);
 
   process.on('SIGINT', () => {
